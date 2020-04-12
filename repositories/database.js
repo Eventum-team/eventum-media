2572be7ea9a8395dbe506048e5df9f6408d8f895
Marcel Martinez <majmartinezva@unal.edu.co>
2020-04-12 17:26:15 -0500

    docker connect fix :whale:

--- repositories/database.js
+++ repositories/database.js
@@ -1,5 +1,30 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/test_db', {
+/*mongoose.connect('mongodb://localhost/test_db', {
     useNewUrlParser: true//revisar en la documentacion
-}).then(db => console.log('DB is connected')).catch(err => console.log(err));
+}).then(db => console.log('DB is connected')).catch(err => console.log(err));*/
+
+const uri = 'mongodb://mongo:27017/image_test'
+
+const db = mongoose.connection;
+
+mongoose.connect(
+    uri,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+).then(()=>console.log("Connected to mongodb"))
+.catch(
+    error => console.log(error)
+);
+
+db.once(
+    'open',
+    _ => { console.log("Database is connected to " + uri) }
+)
+
+db.on(
+    'error',
+    error=>console.log(error)
+)
