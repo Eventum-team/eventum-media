6c72a3c50fcbd54b924086df3bafbb3ce5245687
Marcel Martinez <majmartinezva@unal.edu.co>
2020-04-12 16:59:55 -0500

    dockerfile fix :whale:

--- repositories/database.js
+++ repositories/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/test_db', {
+mongoose.connect('mongodb://localhost/test', {
     useNewUrlParser: true//revisar en la documentacion
 }).then(db => console.log('DB is connected')).catch(err => console.log(err));
