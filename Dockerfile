8bd5e598b87b7a3e6a7af704e9fad2c1800bc602
Marcel Martinez <majmartinezva@unal.edu.co>
2020-04-12 23:59:27 -0500

    virtual :whale:

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-FROM node:latest
-WORKDIR /eventum-media-ms
+FROM node:10
+WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install
 COPY . .
--- repositories/database.js
+++ repositories/database.js
@@ -1,30 +1,11 @@
 const mongoose = require('mongoose');
 
+mongoose
+  .connect(
+    'mongodb://mongo:27017/docker-node-mongo',
+    { useNewUrlParser: true }
+  ).then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
+
 /*mongoose.connect('mongodb://localhost/test_db', {
     useNewUrlParser: true//revisar en la documentacion
 }).then(db => console.log('DB is connected')).catch(err => console.log(err));*/
-
-const uri = 'mongodb://mongo:27017/image_test'
-
-const db = mongoose.connection;
-
-mongoose.connect(
-    uri,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }
-).then(()=>console.log("Connected to mongodb"))
-.catch(
-    error => console.log(error)
-);
-
-db.once(
-    'open',
-    _ => { console.log("Database is connected to " + uri) }
-)
-
-db.on(
-    'error',
-    error=>console.log(error)
-)
