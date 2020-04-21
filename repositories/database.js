0be692e15112f7526e39a430b3b861e996d6fe85
Marcel Martinez <majmartinezva@unal.edu.co>
2020-04-21 09:33:16 -0500

    error Dockerfile :whale:

--- repositories/database.js
+++ repositories/database.js
@@ -2,10 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose
   .connect(
-    'mongodb://localhost/test_db',//for despliegue//'mongodb://mongo:27017/imageDB',
+    'mongodb://mongo:27017/imageDB',//for despliegue//'mongodb://localhost/test_db',
     { useNewUrlParser: true }
   ).then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
-
-/*mongoose.connect('mongodb://localhost/test_db', {
-    useNewUrlParser: true//revisar en la documentacion
-}).then(db => console.log('DB is connected')).catch(err => console.log(err));*/
