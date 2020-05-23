24c7502a6283e12e637b18bb9741a7e27747ff03
icquirogac <icquirogac@unal.edu.co>
2020-05-23 16:32:56 -0500

    con https

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const multer = require('multer');
 const path = require('path');
 const { v4 : uuidv4 } = require('uuid');
 const bodyParser = require("body-parser");
+var https = require('https');
+var fs = require('fs');
 
 // Initialitazion
 const app = express();
@@ -11,6 +13,12 @@ require('./repositories/database');
 // Settings
 app.set('port', 3001);
 app.set('view engine', 'ejs');
+const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+    passphrase: "0000"
+  };
+
 
 // Middleware
 app.use(bodyParser.json()); // <--- Here
@@ -42,7 +50,14 @@ app.use(require('./resources/imageResoure'));
 // static files acceso desde navegador
 app.use(express.static('public'));//testeo
 
+https.createServer(options, (req, res) => {
+    res.writeHead(200);
+    res.end('hello world\n');
+  }).listen(8443);
+  
+
+
 // Start
-app.listen(app.get('port'), () =>{
-    console.log(`Server on port ${app.get('port')}`);
-});
+// app.listen(app.get('port'), () =>{
+//     console.log(`Server on port ${app.get('port')}`);
+// });
