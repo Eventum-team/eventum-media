1d6b27963695d07ec595da5c38ee4e15222a7655
icquirogac <icquirogac@unal.edu.co>
2020-05-23 17:49:39 -0500

    Docker corregido y database para que sirva con docker

--- repositories/database.js
+++ repositories/database.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose
   .connect(
-    //'mongodb://mongo:27017/imageDB',
+    'mongodb://mongo:27017/imageDB',
     //for despliegue
-    'mongodb://localhost/test_db',
+    // 'mongodb://localhost/test_db',
     { useNewUrlParser: true }
   ).then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
