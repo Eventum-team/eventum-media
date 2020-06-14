76dc6919e9200010cddb680853bcce82b38657b3
icquirogac <icquirogac@unal.edu.co>
2020-06-13 20:06:07 -0500

    webworking

---
+++ web/build/precache-manifest.ccea8d119295891675194b5c651765a4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7bea52be49eea9741186c14a11a0b07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ce54396eecb4e13a3d9",
+    "url": "/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "82ac1cd5cbb50f2080b4",
+    "url": "/static/css/main.f38c0693.chunk.css"
+  },
+  {
+    "revision": "0ce54396eecb4e13a3d9",
+    "url": "/static/js/2.e6963c59.chunk.js"
+  },
+  {
+    "revision": "b96169811d680a89988a6f8e7e691d3b",
+    "url": "/static/js/2.e6963c59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82ac1cd5cbb50f2080b4",
+    "url": "/static/js/main.b9bb3395.chunk.js"
+  },
+  {
+    "revision": "dcb2660bf2873822983a",
+    "url": "/static/js/runtime-main.ae5843bb.js"
+  },
+  {
+    "revision": "61cb49522cf2ee83912984b52ab440f6",
+    "url": "/static/media/Component 8 – 1.61cb4952.png"
+  },
+  {
+    "revision": "f78745222cfb77f18273a696c24ccd3c",
+    "url": "/static/media/Homebanner.f7874522.png"
+  },
+  {
+    "revision": "fb7c6df356bfcc724a781078eca6e4c1",
+    "url": "/static/media/eventumIcon.fb7c6df3.png"
+  },
+  {
+    "revision": "f7c031a5a31bb312700b62291568f8d1",
+    "url": "/static/media/priscilla-du-preez-XkKCui44iM0-unsplash.f7c031a5.jpg"
+  },
+  {
+    "revision": "a338d7fc23c6999552902d0b11dad6e4",
+    "url": "/static/media/teemu-paananen-bzdhc5b3Bxs-unsplash.a338d7fc.jpg"
+  },
+  {
+    "revision": "6255734f079d6d7e3bf9490686764e81",
+    "url": "/static/media/undraw_schedule_pnbk.6255734f.png"
+  },
+  {
+    "revision": "ff3d3d85a34d7f541a7a5d1b76a736d5",
+    "url": "/static/media/william-recinos-qtYhAQnIwSE-unsplash.ff3d3d85.jpg"
+  }
+]);
